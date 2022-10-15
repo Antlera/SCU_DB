dcf70323ac39a44e19484eb8635f5adfcc1a83fb
Antlers <3219599757@qq.com>
2022-10-15 16:50:02 +0800

    Q6 Order lags

--- 1/homework1.sql
+++ 1/homework1.sql
@@ -137,6 +137,36 @@ ON
 ORDER BY ProductName;
 
 
+--Q6 [10 POINTS] (Q6_ORDER_LAGS):
+--For the first 10 orders by CutomerId BLONP: get the Order's Id, OrderDate, 
+--previous OrderDate, and difference between the previous and current. Return 
+--results ordered by OrderDate (ascending)
+--Details: The "previous" OrderDate for the first order should default to itself (lag 
+--time = 0). Use the julianday() function for date arithmetic (example).
+--Use lag(expr, offset, default) for grabbing previous dates.
+--Please round the lag time to the nearest hundredth, formatted like 17361|2012-09-
+--19 12:13:21|2012-09-18 22:37:15|0.57\
+SELECT Id,OrderDate,PreDate,ROUND(JULIANDAY(OrderDate) - JULIANDAY(PreDate),2) DiffOrderDate
+FROM
+(SELECT
+	o.Id Id,
+	OrderDate,
+	LAG(OrderDate,1,OrderDate)
+	OVER (
+		ORDER BY
+		OrderDate) PreDate
+FROM
+	"Order" o
+JOIN Customer c ON
+	o.CustomerId = c.id
+WHERE
+	CustomerId = "BLONP"
+ORDER BY
+	OrderDate ASC
+LIMIT 10);
+
+
+
 
 
 SELECT COUNT(*) total FROM "Order" o JOIN Shipper s ON o.ShipVia = s.Id  GROUP BY s.Id T
