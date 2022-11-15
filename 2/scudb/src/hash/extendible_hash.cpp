17e1f9bdae3f0ef2b5a45b88829704087b7fea51
Lan <antlers@3219599757@qq.com>
2022-11-15 12:46:30 +0800

    完成插入函数并保证全局与局部深度正确更新

--- 2/scudb/src/hash/extendible_hash.cpp
+++ 2/scudb/src/hash/extendible_hash.cpp
@@ -1,23 +1,36 @@
 #include <list>
-
+#include <cmath>
+#include <algorithm>
 #include "hash/extendible_hash.h"
 #include "page/page.h"
 
-namespace scudb {
+    namespace scudb {
 
 /*
  * constructor
  * array_size: fixed array size for each bucket
  */
 template <typename K, typename V>
-ExtendibleHash<K, V>::ExtendibleHash(size_t size) {}
-
+ExtendibleHash<K, V>::ExtendibleHash(size_t size) : globalDepth(0), bucketFixedSize(size){
+  buckets.push_back(std::make_shared<Bucket>(0));
+}
+template <typename K, typename V>
+ExtendibleHash<K, V>::ExtendibleHash() : globalDepth(0){
+  ExtendibleHash(SIZE_MAX);
+}
 /*
  * helper function to calculate the hashing address of input key
  */
 template <typename K, typename V>
-size_t ExtendibleHash<K, V>::HashKey(const K &key) {
-  return 0;
+size_t ExtendibleHash<K, V>::HashKey(const K &key) const{
+  return std::hash<K>{}(key);
+}
+/*
+ * helper function to truncate the low bits of hashing address to be the bucket index
+ */
+template <typename K, typename V>
+int ExtendibleHash<K, V>::getBucketIndex(const K &key) const{
+  return HashKey(key) & ((1 << globalDepth) - 1);
 }
 
 /*
@@ -25,8 +38,9 @@ size_t ExtendibleHash<K, V>::HashKey(const K &key) {
  * NOTE: you must implement this function in order to pass test
  */
 template <typename K, typename V>
-int ExtendibleHash<K, V>::GetGlobalDepth() const {
-  return 0;
+int ExtendibleHash<K, V>::GetGlobalDepth() const
+{
+  return globalDepth;
 }
 
 /*
@@ -35,7 +49,13 @@ int ExtendibleHash<K, V>::GetGlobalDepth() const {
  */
 template <typename K, typename V>
 int ExtendibleHash<K, V>::GetLocalDepth(int bucket_id) const {
-  return 0;
+  if(buckets[bucket_id]){//判断对应id的bucket是否存在
+    if(buckets[bucket_id]->hashMap.size()==0)
+      return -1;
+    else
+      return buckets[bucket_id]->localDepth;
+  }
+  return -1;//若不存在返回-1
 }
 
 /*
@@ -43,7 +63,7 @@ int ExtendibleHash<K, V>::GetLocalDepth(int bucket_id) const {
  */
 template <typename K, typename V>
 int ExtendibleHash<K, V>::GetNumBuckets() const {
-  return 0;
+  return bucketNum;
 }
 
 /*
@@ -69,7 +89,61 @@ bool ExtendibleHash<K, V>::Remove(const K &key) {
  * global depth
  */
 template <typename K, typename V>
-void ExtendibleHash<K, V>::Insert(const K &key, const V &value) {}
+void ExtendibleHash<K, V>::Insert(const K &key, const V &value) {
+  int bIndex = getBucketIndex(key);
+  auto cur = buckets[bIndex];
+  size_t bucketSize = (size_t)pow(2,(globalDepth - cur->localDepth));
+  while (true)
+  {
+    if(cur->hashMap.find(key) != cur->hashMap.end())
+    {
+      cur->hashMap[key] = value;
+      break;
+    }
+    else if (cur->hashMap.size() < std::min(bucketSize, bucketFixedSize))
+    {
+      cur->hashMap[key] = value;
+      break;
+    }
+
+    int mask = (1 << (cur->localDepth));//桶大小不足需要分裂
+    cur->localDepth++;
+
+    {
+      if(cur->localDepth){
+
+        size_t length = buckets.size();
+        for (size_t i = 0; i < length; i++){
+          buckets.push_back(buckets[i]);
+        }
+        globalDepth++;
+      }
+      bucketNum++;
+
+      auto newBucket = std::make_shared<Bucket>(cur->localDepth);
+
+      for (auto it = cur->hashMap.begin(); it != cur->hashMap.end();)
+      {
+        if (HashKey(it->first) & mask)
+        {
+          newBucket->hashMap[it->first] = it->second; //  将localDepth下一位上为0与为1的分开
+          it = cur->hashMap.erase(it);
+        }
+        else
+          it++;
+      }
+      for (size_t i = 0; i < buckets.size(); i++)
+      {
+        if (buckets[i] == cur && (i & mask))
+          buckets[i] = newBucket;
+      }
+      bIndex = getBucketIndex(key);
+      cur = buckets[bIndex];//由于globalDepth更新，因此需要重新更新索引位置
+    }
+  }
+  
+  
+}
 
 template class ExtendibleHash<page_id_t, Page *>;
 template class ExtendibleHash<Page *, std::list<Page *>::iterator>;
