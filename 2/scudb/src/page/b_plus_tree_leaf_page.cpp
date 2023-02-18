47d0dfcf40657c13cc12b815164375e72a024344
Lan <antlers@3219599757@qq.com>
2023-02-18 14:38:23 +0800

    leaf page实现

--- 2/scudb/src/page/b_plus_tree_leaf_page.cpp
+++ 2/scudb/src/page/b_plus_tree_leaf_page.cpp
@@ -3,7 +3,7 @@
  */
 
 #include <sstream>
-
+#include <include/page/b_plus_tree_internal_page.h>
 #include "common/exception.h"
 #include "common/rid.h"
 #include "page/b_plus_tree_leaf_page.h"
@@ -20,18 +20,26 @@ namespace scudb {
  * next page id and set max size
  */
 INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_LEAF_PAGE_TYPE::Init(page_id_t page_id, page_id_t parent_id) {}
+void B_PLUS_TREE_LEAF_PAGE_TYPE::Init(page_id_t page_id, page_id_t parent_id) {
+  SetPageType(IndexPageType::LEAF_PAGE);
+  SetSize(0);
+
+  SetPageId(page_id);
+  SetParentPageId(parent_id);
+  SetNextPageId(INVALID_PAGE_ID);
+  SetMaxSize((PAGE_SIZE - sizeof(BPlusTreeLeafPage)) / sizeof(MappingType) - 1); // 同样需要减一
+}
 
 /**
  * Helper methods to set/get next page id
  */
 INDEX_TEMPLATE_ARGUMENTS
 page_id_t B_PLUS_TREE_LEAF_PAGE_TYPE::GetNextPageId() const {
-  return INVALID_PAGE_ID;
+  return next_page_id_;
 }
 
 INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_LEAF_PAGE_TYPE::SetNextPageId(page_id_t next_page_id) {}
+void B_PLUS_TREE_LEAF_PAGE_TYPE::SetNextPageId(page_id_t next_page_id) { next_page_id_ = next_page_id; }
 
 /**
  * Helper method to find the first index i so that array[i].first >= key
@@ -40,7 +48,16 @@ void B_PLUS_TREE_LEAF_PAGE_TYPE::SetNextPageId(page_id_t next_page_id) {}
 INDEX_TEMPLATE_ARGUMENTS
 int B_PLUS_TREE_LEAF_PAGE_TYPE::KeyIndex(
     const KeyType &key, const KeyComparator &comparator) const {
-  return 0;
+  int l = 0, r = GetSize() - 1;
+  while (l <= r) //二分查找小于等于的最大值
+  { 
+    int mid = (r - l) / 2 + l;
+    if (comparator(array[mid].first,key) >= 0)
+      r = mid - 1;
+    else
+      l = mid + 1;
+  }
+  return r + 1;
 }
 
 /*
@@ -49,9 +66,7 @@ int B_PLUS_TREE_LEAF_PAGE_TYPE::KeyIndex(
  */
 INDEX_TEMPLATE_ARGUMENTS
 KeyType B_PLUS_TREE_LEAF_PAGE_TYPE::KeyAt(int index) const {
-  // replace with your own code
-  KeyType key;
-  return key;
+  return array[index].first;
 }
 
 /*
@@ -60,8 +75,7 @@ KeyType B_PLUS_TREE_LEAF_PAGE_TYPE::KeyAt(int index) const {
  */
 INDEX_TEMPLATE_ARGUMENTS
 const MappingType &B_PLUS_TREE_LEAF_PAGE_TYPE::GetItem(int index) {
-  // replace with your own code
-  return array[0];
+  return array[index];
 }
 
 /*****************************************************************************
@@ -75,7 +89,17 @@ INDEX_TEMPLATE_ARGUMENTS
 int B_PLUS_TREE_LEAF_PAGE_TYPE::Insert(const KeyType &key,
                                        const ValueType &value,
                                        const KeyComparator &comparator) {
-  return 0;
+  int index = KeyIndex(key, comparator); 
+  IncreaseSize(1);
+  int curSize = GetSize();
+  for (int i = curSize - 1; i > index; i--)
+  {
+    array[i].first = array[i - 1].first;
+    array[i].second = array[i - 1].second;
+  }
+  array[index].first = key;
+  array[index].second = value;
+  return curSize;
 }
 
 /*****************************************************************************
@@ -87,7 +111,21 @@ int B_PLUS_TREE_LEAF_PAGE_TYPE::Insert(const KeyType &key,
 INDEX_TEMPLATE_ARGUMENTS
 void B_PLUS_TREE_LEAF_PAGE_TYPE::MoveHalfTo(
     BPlusTreeLeafPage *recipient,
-    __attribute__((unused)) BufferPoolManager *buffer_pool_manager) {}
+    __attribute__((unused)) BufferPoolManager *buffer_pool_manager) {
+  assert(recipient != nullptr);
+  int total = GetMaxSize() + 1;
+  int copyIndex = (total) / 2;
+  for (int i = copyIndex; i < total; i++)
+  {
+    recipient->array[i - copyIndex].first = array[i].first;
+    recipient->array[i - copyIndex].second = array[i].second;
+  }
+  recipient->SetNextPageId(GetNextPageId());
+  SetNextPageId(recipient->GetPageId());
+  SetSize(copyIndex);
+  recipient->SetSize(total - copyIndex);
+
+}
 
 INDEX_TEMPLATE_ARGUMENTS
 void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyHalfFrom(MappingType *items, int size) {}
@@ -103,6 +141,11 @@ void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyHalfFrom(MappingType *items, int size) {}
 INDEX_TEMPLATE_ARGUMENTS
 bool B_PLUS_TREE_LEAF_PAGE_TYPE::Lookup(const KeyType &key, ValueType &value,
                                         const KeyComparator &comparator) const {
+  int index = KeyIndex(key, comparator);
+  if (index < GetSize() && comparator(array[index].first, key) == 0){
+    value = array[index].second;
+    return true;
+  }
   return false;
 }
 
@@ -118,7 +161,16 @@ bool B_PLUS_TREE_LEAF_PAGE_TYPE::Lookup(const KeyType &key, ValueType &value,
 INDEX_TEMPLATE_ARGUMENTS
 int B_PLUS_TREE_LEAF_PAGE_TYPE::RemoveAndDeleteRecord(
     const KeyType &key, const KeyComparator &comparator) {
-  return 0;
+  int firIndexLargerEqualThanKey = KeyIndex(key,comparator);
+  if (firIndexLargerEqualThanKey >= GetSize() || comparator(key,KeyAt(firIndexLargerEqualThanKey)) != 0) {
+    return GetSize();
+  }
+  
+  int tarIndex = firIndexLargerEqualThanKey;
+  memmove(array + tarIndex, array + tarIndex + 1,
+          static_cast<size_t>((GetSize() - tarIndex - 1) * sizeof(MappingType)));
+  IncreaseSize(-1);
+  return GetSize();
 }
 
 /*****************************************************************************
@@ -130,7 +182,17 @@ int B_PLUS_TREE_LEAF_PAGE_TYPE::RemoveAndDeleteRecord(
  */
 INDEX_TEMPLATE_ARGUMENTS
 void B_PLUS_TREE_LEAF_PAGE_TYPE::MoveAllTo(BPlusTreeLeafPage *recipient,
-                                           int, BufferPoolManager *) {}
+                                           int, BufferPoolManager *) {
+  int startIndex = recipient->GetSize();
+  for (int i = 0; i < GetSize(); i++)
+  {
+    recipient->array[startIndex + i].first = array[i].first;
+    recipient->array[startIndex + i].second = array[i].second;
+  }
+  recipient->SetNextPageId(GetNextPageId());
+  recipient->IncreaseSize(GetSize());
+  SetSize(0);
+}
 INDEX_TEMPLATE_ARGUMENTS
 void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyAllFrom(MappingType *items, int size) {}
 
@@ -144,10 +206,22 @@ void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyAllFrom(MappingType *items, int size) {}
 INDEX_TEMPLATE_ARGUMENTS
 void B_PLUS_TREE_LEAF_PAGE_TYPE::MoveFirstToEndOf(
     BPlusTreeLeafPage *recipient,
-    BufferPoolManager *buffer_pool_manager) {}
+    BufferPoolManager *buffer_pool_manager) {
+  MappingType pair = GetItem(0);
+  IncreaseSize(-1);
+  memmove(array, array + 1, static_cast<size_t>(GetSize() * sizeof(MappingType)));
+  recipient->CopyLastFrom(pair);
+  Page *page = buffer_pool_manager->FetchPage(GetParentPageId());
+  B_PLUS_TREE_INTERNAL_PAGE *parent = reinterpret_cast<B_PLUS_TREE_INTERNAL_PAGE *>(page->GetData());
+  parent->SetKeyAt(parent->ValueIndex(GetPageId()), array[0].first);
+  buffer_pool_manager->UnpinPage(GetParentPageId(), true);
+}
 
 INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyLastFrom(const MappingType &item) {}
+void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyLastFrom(const MappingType &item) {
+  array[GetSize()] = item;
+  IncreaseSize(1);
+}
 /*
  * Remove the last key & value pair from this page to "recipient" page, then
  * update relavent key & value pair in its parent page.
@@ -155,12 +229,26 @@ void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyLastFrom(const MappingType &item) {}
 INDEX_TEMPLATE_ARGUMENTS
 void B_PLUS_TREE_LEAF_PAGE_TYPE::MoveLastToFrontOf(
     BPlusTreeLeafPage *recipient, int parentIndex,
-    BufferPoolManager *buffer_pool_manager) {}
+    BufferPoolManager *buffer_pool_manager) {
+  MappingType pair = GetItem(GetSize() - 1);
+  IncreaseSize(-1);
+  recipient->CopyFirstFrom(pair, parentIndex, buffer_pool_manager);
+}
 
 INDEX_TEMPLATE_ARGUMENTS
 void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyFirstFrom(
     const MappingType &item, int parentIndex,
-    BufferPoolManager *buffer_pool_manager) {}
+    BufferPoolManager *buffer_pool_manager) {
+
+  memmove(array + 1, array, GetSize()*sizeof(MappingType));
+  IncreaseSize(1);
+  array[0] = item;
+
+  Page *page = buffer_pool_manager->FetchPage(GetParentPageId());
+  B_PLUS_TREE_INTERNAL_PAGE *parent = reinterpret_cast<B_PLUS_TREE_INTERNAL_PAGE *>(page->GetData());
+  parent->SetKeyAt(parentIndex, array[0].first);
+  buffer_pool_manager->UnpinPage(GetParentPageId(), true);
+}
 
 /*****************************************************************************
  * DEBUG
